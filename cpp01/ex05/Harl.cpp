dcf98d18c1391452ed3f50fcb38822005b930a3a
faridnasrdine <farid@AKAGAMI>
2025-10-27 16:19:23 +0100

    good

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,18 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+Harl::~Harl() {}
+void Harl::debug( void ) {
+    std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+void Harl::info( void ) {
+    std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+void Harl::warning( void ) {
+    std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
+}
+void Harl::error( void ) {
+    std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now.\n";
+}
+
+
